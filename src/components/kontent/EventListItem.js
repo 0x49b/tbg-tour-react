df40f8da15a6df847be753e083119c3c22a1a9e6
Florian Thiévent <florian.thievent@sbb.ch>
2022-11-03 18:25:51 +0100

    some saving

--- src/components/kontent/EventListItem.js
+++ src/components/kontent/EventListItem.js
@@ -2,7 +2,26 @@ import React from 'react'
 import './event.css'
 
 const EventListItem = ({ details }) => {
-  return <li>{details.name} {details.play}</li>
+  return (
+    <li className="event-list-item">
+      <div className="event-list-item-header">
+        <div className="event-list-item-title">{details.name}</div>
+          
+          {details.cert !== null ? (
+            <div className="event-list-pill">{details.cert}</div>
+          ) : (
+            ''
+          )}
+        
+      </div>
+
+      <div className='event-list-body'>
+      {details.ca_play ? `ca. ${details.play}` : details.play}
+      </div>
+
+      
+    </li>
+  )
 }
 
-export default EventListItem;
+export default EventListItem
